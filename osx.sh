404239975032ea570505c46a2e7e779f8e2f3faf
Robert Richards <work@robertrichards.me>
2016-12-19 22:25:49 -0500

    fixed keyboard delsy

--- osx.sh
+++ osx.sh
@@ -197,5 +197,5 @@ hash tmutil &> /dev/null && sudo tmutil disablelocal
 defaults write com.apple.finder AppleShowAllFiles YES
 
 # Key repeat
-defaults write -g InitialKeyRepeat -int 1 # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 2 # normal minimum is 2 (30 ms)
